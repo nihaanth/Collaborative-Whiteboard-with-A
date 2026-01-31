8749fdc2f5f1566086b39c193177ab44d9fb3ce9
nihaanth <nihaanthreddy3@gmail.com>
2026-01-30 21:37:12 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+// @ts-ignore
+import CanvasPaint from 'react-canvas-paint';
+
+import 'react-canvas-paint/dist/index.css'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+    
+      </div>
+      <h1>Collab Board with AI</h1>
+
+      <CanvasPaint />
+    </>
+  )
+}
+
+export default App
