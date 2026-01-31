3c0aa495c2e0b92850c6389b8b19288d82a51663
nihaanth <nihaanthreddy3@gmail.com>
2026-01-31 12:23:35 -0600

    added drawing feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,54 @@
-import { useState } from 'react'
-// @ts-ignore
-import CanvasPaint from 'react-canvas-paint';
-
-import 'react-canvas-paint/dist/index.css'
-import './App.css'
+import { useRef,useState } from 'react'
 
 function App() {
+  // 1. Create the reference container
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  const [isDrawing, setIsDrawing] = useState(false)
+
+  const startDrawing = (event: React.MouseEvent) => {
+    setIsDrawing(true)
+    const canvas = canvasRef.current
+    if (canvas) {
+      const ctx = canvas.getContext('2d')
+      if (ctx) {
+        ctx.beginPath()
+        ctx.moveTo(event.nativeEvent.offsetX, event.nativeEvent.offsetY)
+      }
+    }
+  }
+
+  const draw = (event: React.MouseEvent) => {
+    if (!isDrawing) return
+    const canvas = canvasRef.current
+    if (canvas) {
+      const ctx = canvas.getContext('2d')
+      if (ctx) {
+        ctx.lineTo(event.nativeEvent.offsetX, event.nativeEvent.offsetY)
+        ctx.stroke()
+      }
+    }
+  }
 
 
   return (
     <>
-      <div>
-    
-      </div>
       <h1>Collab Board with AI</h1>
+      
 
-      <CanvasPaint />
+      <canvas 
+        ref={canvasRef}        // Attach our reference to this DOM element
+        width={800}            
+        height={600}  
+        onMouseDown={startDrawing}
+        onMouseMove={draw}       
+        onMouseUp={() => setIsDrawing(false)}
+        onMouseLeave={() => setIsDrawing(false)}  
+        style={{ 
+          border: '1px solid black',
+          backgroundColor: 'white'
+        }} 
+      />
     </>
   )
 }
